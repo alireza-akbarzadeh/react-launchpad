3c97465df14fb90765db12e03bc464cfb782ee4f
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2024-03-22 12:48:58 +0330

    format data

---
+++ src/containers/client-only.tsx
@@ -0,0 +1,21 @@
+import { useIsMounted } from "hooks";
+import { PropsWithChildren } from "react";
+
+function ClientOnly<T>(props: PropsWithChildren<T>) {
+  const { children, ...rest } = props;
+  const isMounted = useIsMounted();
+  if (!isMounted) return <div {...rest} />;
+  return <>{children}</>;
+}
+export default ClientOnly;
+
+//? another version
+// export default function ClientOnly<T>(props: PropsWithChildren<T>) {
+//   const { children, ...rest } = props
+//   const [hasMounted, setHasMounted] = useState(false)
+//   useEffect(() => {
+//     setHasMounted(true)
+//   }, [])
+//   if (!hasMounted) return <div {...rest} />
+//   return <>{props.children}</>
+// }

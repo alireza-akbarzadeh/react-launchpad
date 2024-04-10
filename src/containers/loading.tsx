59f82d5678c9d2e79290fc59e75dfa128148256f
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2024-04-10 23:58:42 +0330

    modify loading components

--- src/containers/loading.tsx
+++ src/containers/loading.tsx
@@ -1,3 +1,62 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 export const Loading = () => {
-  return <div>Loading...</div>;
+  useGSAP(() => {
+    gsap.from("#upper .loading_Circle", {
+      y: -40,
+      duration: 0.5,
+      stagger: {
+        each: 0.2,
+        repeat: -1,
+        yoyo: true,
+      },
+      ease: "power1.in",
+    });
+  });
+  useGSAP(() => {
+    gsap.from("#downer .loading_Circle", {
+      y: 60,
+      duration: 0.5,
+      stagger: {
+        each: 0.2,
+        repeat: -1,
+        yoyo: true,
+      },
+      ease: "power1.in",
+      opacity: 0,
+    });
+  });
+
+  useGSAP(() => {
+    gsap.to("#bounce", {
+      yPercent: -100,
+      stagger: {
+        each: 0.3,
+      },
+      ease: "power4.out",
+      duration: 0.5,
+      paused: true,
+    });
+  }, []);
+
+  return (
+    <>
+      <div id="bounce" className="loading_container">
+        <div id="upper" className="flex">
+          <div className="loading_Circle"></div>
+          <div className="loading_Circle"></div>
+          <div className="loading_Circle"></div>
+        </div>
+        <div id="downer" className="flex">
+          <div className="loading_Circle"></div>
+          <div className="loading_Circle"></div>
+          <div className="loading_Circle"></div>
+        </div>
+      </div>
+      <div id="bounce" className="loading_container cyan"></div>
+      <div id="bounce" className="loading_container darkblue"></div>
+      <div id="bounce" className="loading_container cyan"></div>
+    </>
+  );
 };

39020a4da4cf08c605fe583a91bf4e9ca390d1ea
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2024-04-21 18:40:14 +0330

    install msw

--- tests/components/main.test.tsx
+++ tests/components/main.test.tsx
@@ -3,7 +3,6 @@ import { describe, expect, it } from 'vitest';
 describe('isRouteErrorResponse', () => {
   it('signal error response', async () => {
     const valuse = { name: 'mosh' };
-
     expect(valuse).toEqual({ name: 'mosh' });
   });
 });

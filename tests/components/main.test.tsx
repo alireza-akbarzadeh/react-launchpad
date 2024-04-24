d034a8b8669af9b6a266fe374fd116aaa95b1de3
Alireza Akbarzadeh <Akbarzadeh.Ali@hasti.co>
2024-04-24 10:32:53 +0330

    resolve lint error

--- tests/components/main.test.tsx
+++ tests/components/main.test.tsx
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest';
 
 describe('isRouteErrorResponse', () => {
-  it('signal error response', async () => {
+  it('signal error response', () => {
     const valuse = { name: 'mosh' };
     expect(valuse).toEqual({ name: 'mosh' });
   });
